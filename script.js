11c55e2244d61d1b695796792bc5379762ad1188
Griffin <80220535+Glloyd5@users.noreply.github.com>
2023-10-19 16:34:39 -0600

    Add feat rainbow coloring
    add features: rainbow coloring, eraser.
    add style option buttons.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let grid = document.querySelector('.grid');
 let squares = document.querySelectorAll('.squares');
+let currentColor = 'black';
+const randomColors = [];
 
 function createGrid (gridSize) { 
     let gridArea = gridSize * gridSize;
@@ -8,35 +10,28 @@ function createGrid (gridSize) {
         squares.classList.add('squares');
         grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
         grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-        grid.appendChild(squares);
-    } 
-    document.querySelectorAll('.squares').forEach(item => {
-        item.addEventListener('mouseover', () => {
-          item.style.backgroundColor = 'black';
-        })
-      })
+        grid.appendChild(squares); 
+}
 }
 
-let gridClear = function() {
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; 
+slider.oninput = function() {
+output.innerHTML = `${this.value} x ${this.value}`;
+}
+
+const gridClear = function() {
   document.querySelectorAll('.squares').forEach(item => {
     item.style.backgroundColor = '#ecdaa8' 
 });
 }
 
-
-document.querySelectorAll('.squares').forEach(item => {
-    item.addEventListener('mouseover', () => {
-      item.style.backgroundColor = 'black';
-    })
-  })
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; 
-  
-  
-slider.oninput = function() {
-output.innerHTML = this.value;
+const generateRainbowColors = function() {
+  for (let i = 0; i < 100; i++) {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    randomColors.push(randomColor);
+  }  
 }
 
 document.querySelector('.slidecontainer').addEventListener('mouseup', () => {
@@ -48,5 +43,32 @@ document.getElementById('clearButton').addEventListener('click',() => {
   gridClear();
 });
 
+document.getElementById('eraserButton').addEventListener('click',() => {
+  document.querySelector('.grid').addEventListener('mouseover', e => {
+    currentColor = '#ecdaa8';
+    e.target.style.backgroundColor = currentColor;
+  }, {capture: true})   
+});
+
+document.getElementById('blackButton').addEventListener('click',() => {
+  document.querySelector('.grid').addEventListener('mouseover', e => {
+    currentColor = 'black';
+    e.target.style.backgroundColor = currentColor;
+  }, {capture: true})  
+});
+
+document.getElementById('rainbowButton').addEventListener('click',() => {
+  document.querySelector('.grid').addEventListener('mouseover', e => {
+    e.target.style.backgroundColor = randomColors[Math.floor(Math.random() * 100)];
+  }, {capture: true})
+});
+
+document.querySelector('.grid').addEventListener('mouseover', e => {
+  e.target.style.backgroundColor = currentColor;
+}, {capture: true}) 
 
 createGrid(4);
+generateRainbowColors();
+
+
+
