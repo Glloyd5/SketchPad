ab60b251a8d651be27b3ff0c6559a97d86ad425d
Griffin <80220535+Glloyd5@users.noreply.github.com>
2023-10-20 13:16:22 -0600

    Add feat custom color picker
    custom color picker is coloris link in README.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # SketchPad
 
-Body background image by Dan Cristian Pădureț: https://unsplash.com/@dancristianpaduret?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash 
+Body background image by Dan Cristian Pădureț: 
+https://unsplash.com/@dancristianpaduret?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash 
+
+Color picker is Coloris:
+https://github.com/mdbassit/Coloris/tree/main
   
 
-Create a sketchpad by combining many different new skills learnt throughout The Odin Project Foundations course.
+Create a sketchpad by combining many different skills
+learnt throughout The Odin Project Foundations course.
--- script.js
+++ script.js
@@ -1,74 +1,99 @@
 let grid = document.querySelector('.grid');
 let squares = document.querySelectorAll('.squares');
+let slider = document.getElementById('myRange');
+let output = document.getElementById('demo');
 let currentColor = 'black';
+let customColor = document.getElementById('colorIs');
 const randomColors = [];
 
-function createGrid (gridSize) { 
-    let gridArea = gridSize * gridSize;
-    for (let i = 1; i <= gridArea; i++) {
-        let squares = document.createElement('div');
-        squares.classList.add('squares');
-        grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-        grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-        grid.appendChild(squares); 
-}
-}
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; 
-slider.oninput = function() {
-output.innerHTML = `${this.value} x ${this.value}`;
+function createGrid(gridSize) {
+  let gridArea = gridSize * gridSize;
+  for (let i = 1; i <= gridArea; i++) {
+    let squares = document.createElement('div');
+    squares.classList.add('squares');
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    grid.appendChild(squares);
+  }
 }
 
-const gridClear = function() {
-  document.querySelectorAll('.squares').forEach(item => {
-    item.style.backgroundColor = '#ecdaa8' 
-});
-}
+output.innerHTML = `${slider.value} x ${slider.value}`;
+slider.oninput = function () {
+  output.innerHTML = `${this.value} x ${this.value}`;
+};
 
-const generateRainbowColors = function() {
+const gridClear = function () {
+  document.querySelectorAll('.squares').forEach((item) => {
+    item.style.backgroundColor = '#ecdaa8';
+  });
+};
+
+const generateRainbowColors = function () {
   for (let i = 0; i < 100; i++) {
-    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     randomColors.push(randomColor);
-  }  
-}
+  }
+};
 
 document.querySelector('.slidecontainer').addEventListener('mouseup', () => {
   gridClear();
   createGrid(slider.value);
 });
 
-document.getElementById('clearButton').addEventListener('click',() => {
+document.getElementById('clearButton').addEventListener('click', () => {
   gridClear();
 });
 
-document.getElementById('eraserButton').addEventListener('click',() => {
-  document.querySelector('.grid').addEventListener('mouseover', e => {
-    currentColor = '#ecdaa8';
+customColor.addEventListener('click', () => {
+  document.querySelector('.grid').addEventListener(
+    'mouseover',
+    (e) => {
+      e.target.style.backgroundColor = customColor.value;
+    },
+    { capture: true }
+  );
+});
+
+document.getElementById('eraserButton').addEventListener('click', () => {
+  document.querySelector('.grid').addEventListener(
+    'mouseover',
+    (e) => {
+      currentColor = '#ecdaa8';
+      e.target.style.backgroundColor = currentColor;
+    },
+    { capture: true }
+  );
+});
+
+document.getElementById('blackButton').addEventListener('click', () => {
+  document.querySelector('.grid').addEventListener(
+    'mouseover',
+    (e) => {
+      currentColor = 'black';
+      e.target.style.backgroundColor = currentColor;
+    },
+    { capture: true }
+  );
+});
+
+document.getElementById('rainbowButton').addEventListener('click', () => {
+  document.querySelector('.grid').addEventListener(
+    'mouseover',
+    (e) => {
+      e.target.style.backgroundColor =
+        randomColors[Math.floor(Math.random() * 100)];
+    },
+    { capture: true }
+  );
+});
+
+document.querySelector('.grid').addEventListener(
+  'mouseover',
+  (e) => {
     e.target.style.backgroundColor = currentColor;
-  }, {capture: true})   
-});
-
-document.getElementById('blackButton').addEventListener('click',() => {
-  document.querySelector('.grid').addEventListener('mouseover', e => {
-    currentColor = 'black';
-    e.target.style.backgroundColor = currentColor;
-  }, {capture: true})  
-});
-
-document.getElementById('rainbowButton').addEventListener('click',() => {
-  document.querySelector('.grid').addEventListener('mouseover', e => {
-    e.target.style.backgroundColor = randomColors[Math.floor(Math.random() * 100)];
-  }, {capture: true})
-});
-
-document.querySelector('.grid').addEventListener('mouseover', e => {
-  e.target.style.backgroundColor = currentColor;
-}, {capture: true}) 
+  },
+  { capture: true }
+);
 
 createGrid(4);
 generateRainbowColors();
-
-
-
